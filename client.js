679c6ac188f27078552de7608ec7bfe47f222d7d
irfaan888 <47710948+irfaan888@users.noreply.github.com>
2019-11-26 16:12:39 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,7 @@
 console.log("client is started");
 console.log("client is started to check staging function");
 console.log("line added on github");
+console.log("line added on g1");
+console.log("line added on g2");
+console.log("line added on g3");
 
